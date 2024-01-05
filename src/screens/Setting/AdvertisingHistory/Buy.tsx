3a79e2bb2df0c56dc6c960945824f633031c4a2a
Mai Xuân Phi <101462441+MaiXuanPhi001@users.noreply.github.com>
2024-01-05 10:37:55 +0700

    Add react-native-check-box package

--- src/screens/Setting/AdvertisingHistory/Buy.tsx
+++ src/screens/Setting/AdvertisingHistory/Buy.tsx
@@ -1,4 +1,4 @@
-import { FlatList } from 'react-native'
+import { FlatList, View, Text } from 'react-native'
 import React, { useEffect, memo, useState } from 'react'
 import { getListAdsBuyToUser } from '@utils/userCallApi'
 import { getListAdsBuyPenddingToUser } from '@utils/userCallApi'
@@ -6,16 +6,22 @@ import LottieView from 'lottie-react-native'
 import { colors } from '@themes/colors'
 import { ActivityIndicator } from 'react-native'
 import AdvertisingItem from './AdvertisingItem'
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { useTranslation } from 'react-i18next';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { fonts } from '@themes/fonts'
 
 const Buy = () => {
+    const { t } = useTranslation()
     const [data, setData] = useState<any[]>([]);
     const [page, setPage] = useState(1);
     const [loading, setLoading] = useState(false);
     const [hasMore, setHasMore] = useState(true);
+    const [isChecked, setIsChecked] = useState(false);
 
-    useEffect(() => {
-        loadMoreData();
-    }, []);
+    // useEffect(() => {
+    //     loadMoreData();
+    // }, []);
 
     const refreshData = async () => {
         setLoading(true);
@@ -24,15 +30,15 @@ const Buy = () => {
         setData([]);
         const response = await getListAdsBuyToUser({ page: 1, limit: 10 });
         if (Array.isArray(response?.data?.array)) {
-          setData(response.data.array);
-          if (response.data.array.length === 0) {
-            setHasMore(false);
-          }
+            setData(response.data.array);
+            if (response.data.array.length === 0) {
+                setHasMore(false);
+            }
         } else {
-          console.error('response.data.array is not an array:', response?.data?.array);
+            console.error('response.data.array is not an array:', response?.data?.array);
         }
         setLoading(false);
-      };
+    };
 
     const loadMoreData = async () => {
         if (!loading && hasMore) {
@@ -54,28 +60,109 @@ const Buy = () => {
         }
     };
 
+    const loadMoreDataPending = async () => {
+        if (!loading && hasMore) {
+            setLoading(true);
+            const response = await getListAdsBuyPenddingToUser({
+                limit: 10,
+                page,
+            });
+            if (Array.isArray(response?.data?.array)) {
+                setData(prevData => [...prevData, ...response.data.array]);
+                if (response.data.array.length === 0) {
+                    setHasMore(false);
+                }
+            } else {
+                console.error('response.data.array is not an array:', response?.data?.array);
+            }
+            setPage(page + 1);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if (isChecked) {
+            loadMoreDataPending()
+        } else {
+            loadMoreData()
+        }
+    }, [isChecked])
+
+
     if (data.length === 0) {
         return (
-            <LottieView
-                source={require('../../../assets/lottie/loading.json')}
-                autoPlay
-                loop
-                style={{ alignSelf: 'center', width: 200, height: 200, marginTop: 200 }}
-            />
+            <>
+                <View style={{
+                    flexDirection: 'row',
+                    alignSelf: 'flex-end',
+                    position: 'absolute',
+                    marginTop: hp('4%'),
+                    alignItems: 'center',
+                }}>
+                    <BouncyCheckbox
+                        size={25}
+                        fillColor={colors.blue}
+                        unfillColor="#FFFFFF"
+                        iconStyle={{ borderColor: colors.blue }}
+                        onPress={
+                            () => {
+                                setIsChecked(!isChecked)
+                                setData([])
+                                setPage(1)
+                                setHasMore(true)
+                            }
+                        }
+                        isChecked={isChecked}
+                    />
+                    <Text style={{ fontFamily: fonts.JR }}>{t('Pending')}</Text>
+                </View>
+                <LottieView
+                    source={require('../../../assets/lottie/searchNodata.json')}
+                    autoPlay
+                    loop
+                    style={{ alignSelf: 'center', width: wp('90%'), height: hp('50%'), marginTop: hp('10%') }}
+                />
+            </>
         );
     }
 
     return (
-        <FlatList
-            showsVerticalScrollIndicator={false}
-            style={{ marginTop: 15, marginBottom: 240 }}
-            data={data}
-            keyExtractor={(item) => item.id.toString()}
-            onEndReached={loadMoreData}
-            onEndReachedThreshold={0.1}
-            ListFooterComponent={() => loading && hasMore && <ActivityIndicator size="large" color={colors.blue} />}
-            renderItem={({ item }) => <AdvertisingItem item={item} refreshData={refreshData} />}
-        />
+        <>
+            <View style={{
+                flexDirection: 'row',
+                alignSelf: 'flex-end',
+                position: 'absolute',
+                marginTop: hp('4%'),
+                alignItems: 'center',
+            }}>
+                <BouncyCheckbox
+                    size={25}
+                    fillColor={colors.blue}
+                    unfillColor="#FFFFFF"
+                    iconStyle={{ borderColor: colors.blue }}
+                    onPress={
+                        () => {
+                            setIsChecked(!isChecked)
+                            setData([])
+                            setPage(1)
+                            setHasMore(true)
+                        }
+                    }
+                    isChecked={isChecked}
+                />
+                <Text style={{ fontFamily: fonts.JR }}>{t('Pending')}</Text>
+            </View>
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                style={{ marginTop: 15, marginBottom: 240 }}
+                data={data}
+                keyExtractor={(item) => item.id.toString()}
+                onEndReached={loadMoreData}
+                onEndReachedThreshold={0.1}
+                ListFooterComponent={() => loading && hasMore && <ActivityIndicator size="large" color={colors.blue} />}
+                renderItem={({ item }) => <AdvertisingItem item={item} refreshData={refreshData} />}
+            />
+        </>
     )
 }
 
--- src/screens/Setting/AdvertisingHistory/Sell.tsx
+++ src/screens/Setting/AdvertisingHistory/Sell.tsx
@@ -1,4 +1,4 @@
-import { FlatList } from 'react-native'
+import { FlatList, View, Text } from 'react-native'
 import React, { useEffect, memo, useState } from 'react'
 import { getListAdsSellToUser } from '@utils/userCallApi'
 import { getListAdsSellPenddingToUser } from '@utils/userCallApi'
@@ -6,16 +6,18 @@ import LottieView from 'lottie-react-native'
 import { colors } from '@themes/colors'
 import { ActivityIndicator } from 'react-native'
 import AdvertisingItem from './AdvertisingItem'
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+import { useTranslation } from 'react-i18next';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { fonts } from '@themes/fonts'
 
 const Sell = () => {
+    const { t } = useTranslation()
     const [data, setData] = useState<any[]>([]);
     const [page, setPage] = useState(1);
     const [loading, setLoading] = useState(false);
     const [hasMore, setHasMore] = useState(true);
-
-    useEffect(() => {
-        loadMoreData();
-    }, []);
+    const [isChecked, setIsChecked] = useState(false);
 
     const loadMoreData = async () => {
         if (!loading && hasMore) {
@@ -37,28 +39,112 @@ const Sell = () => {
         }
     };
 
+    const loadMoreDataPending = async () => {
+        if (!loading && hasMore) {
+            setLoading(true);
+            const response = await getListAdsSellPenddingToUser({
+                limit: 10,
+                page,
+            });
+            if (Array.isArray(response?.data?.array)) {
+                setData(prevData => [...prevData, ...response.data.array]);
+                if (response.data.array.length === 0) {
+                    setHasMore(false);
+                }
+            } else {
+                console.error('response.data.array is not an array:', response?.data?.array);
+            }
+            setPage(page + 1);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if (isChecked) {
+            loadMoreDataPending()
+        } else {
+            loadMoreData()
+        }
+    }, [isChecked])
+
     if (data.length === 0) {
         return (
-            <LottieView
-                source={require('../../../assets/lottie/loading.json')}
-                autoPlay
-                loop
-                style={{ alignSelf: 'center', width: 200, height: 200, marginTop: 200 }}
-            />
+            <>
+                <View style={{
+                    flexDirection: 'row',
+                    alignSelf: 'flex-end',
+                    position: 'absolute',
+                    marginTop: hp('4%'),
+                    alignItems: 'center',
+                }}>
+                    <BouncyCheckbox
+                        size={25}
+                        fillColor={colors.blue}
+                        unfillColor="#FFFFFF"
+                        iconStyle={{ borderColor: colors.blue }}
+                        onPress={
+                            () => {
+                                setIsChecked(!isChecked)
+                                setData([])
+                                setPage(1)
+                                setHasMore(true)
+                            }
+                        }
+                        isChecked={isChecked}
+                    />
+                    <Text style={{ fontFamily: fonts.JR }}>{t('Pending')}</Text>
+                </View>
+                <LottieView
+                    source={require('../../../assets/lottie/searchNodata.json')}
+                    autoPlay
+                    loop
+                    style={{ alignSelf: 'center', width: wp('90%'), height: hp('50%'), marginTop: hp('10%') }}
+                />
+            </>
         );
     }
 
     return (
-        <FlatList
-            showsVerticalScrollIndicator={false}
-            style={{ marginTop: 15, marginBottom: 240 }}
-            data={data}
-            keyExtractor={(item) => item.id.toString()}
-            onEndReached={loadMoreData}
-            onEndReachedThreshold={0.1}
-            ListFooterComponent={() => loading && hasMore && <ActivityIndicator size="large" color={colors.blue} />}
-            renderItem={({ item }) => <AdvertisingItem item={item} />}
-        />
+        <>
+            <View style={{
+                flexDirection: 'row',
+                alignSelf: 'flex-end',
+                position: 'absolute',
+                marginTop: hp('4%'),
+                alignItems: 'center',
+            }}>
+                <BouncyCheckbox
+                    size={25}
+                    fillColor={colors.blue}
+                    unfillColor="#FFFFFF"
+                    iconStyle={{ borderColor: colors.blue }}
+                    onPress={
+                        () => {
+                            setIsChecked(!isChecked)
+                            setData([])
+                            setPage(1)
+                            setHasMore(true)
+                        }
+                    }
+                    isChecked={isChecked}
+                />
+                <Text style={{ fontFamily: fonts.JR }}>{t('Pending')}</Text>
+            </View>
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                style={{
+                    marginTop: hp('2%'),
+                    marginBottom: hp('30%'),
+                }}
+                data={data}
+                keyExtractor={(item) => item.id.toString()}
+                // onEndReached={loadMoreData}
+                onEndReached={isChecked ? loadMoreDataPending : loadMoreData}
+                onEndReachedThreshold={0.1}
+                ListFooterComponent={() => loading && hasMore && <ActivityIndicator size="large" color={colors.blue} />}
+                renderItem={({ item }) => <AdvertisingItem item={item} />}
+            />
+        </>
     )
 }
 
