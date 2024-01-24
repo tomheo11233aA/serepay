7ccb12a27209d3a1f38094b9af06e51c2c1bc8b7
faIIinlove <phucnamvan@gmail.com>
2024-01-24 09:00:45 +0700

    Tối ưu hoá CoinChoosed

--- src/screens/TopTabWallet/P2p/CoinChoosed.tsx
+++ src/screens/TopTabWallet/P2p/CoinChoosed.tsx
@@ -4,12 +4,11 @@ import Icon from '@commom/Icon'
 import Txt from '@commom/Txt'
 import { colors } from '@themes/colors'
 import { fonts } from '@themes/fonts'
-import React, { useEffect } from 'react'
+import React, { useEffect, useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
 import { ICoin } from '@models/coin'
 import { keys } from '@contants/keys'
 import CoinModal from '@commom/Modal/CoinModal'
-import { useCallback } from 'react'
 import { setConnected } from '@redux/slice/coinSlice'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '@redux/store/store'
@@ -22,11 +21,17 @@ const CoinChoosed: React.FC<Props> = ({ setSelectedCoin, selectedCoin }) => {
     const { t } = useTranslation()
     const dispatch: AppDispatch = useDispatch()
     const [visible, setVisible] = React.useState(false);
-    const showModal = () => setVisible(true);
-    const hideModal = () => {
-        setVisible(false);
+    // const showModal = () => setVisible(true);
+    // const hideModal = () => {
+    //     setVisible(false);
+    //     dispatch(setConnected(true))
+    // }
+
+    const showModal = useCallback(() => setVisible(true), [])
+    const hideModal = useCallback(() => {
+        setVisible(false)
         dispatch(setConnected(true))
-    }
+    }, [dispatch])
     // const handleChooseCoin = useCallback((coin: ICoin) => {
     //     setSelectedCoin(coin);
     //     hideModal();
@@ -53,7 +58,7 @@ const CoinChoosed: React.FC<Props> = ({ setSelectedCoin, selectedCoin }) => {
                         source={selectedCoin ? { uri: `${keys.HOSTING_API}${selectedCoin.image}` } : require('@images/wallet/bitcoin.png')}
                     />
                     <Txt fontFamily={fonts.IBMPM}>
-                        {selectedCoin ? selectedCoin.token_key : 'Bitcoin'}
+                        {selectedCoin ? selectedCoin.token_key : 'USDT'}
                     </Txt>
                 </Box>
 
